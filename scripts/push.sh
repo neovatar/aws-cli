9caccd64d5114c602f940b40d003bb00f29b4254
Thomas Seliger <tom@neovatar.org>
2021-06-10 13:39:19 +0200

    Fix tagging cmd

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,5 +6,5 @@ docker push neovatar/aws-cli:latest
 
 cd "${GITHUB_WORKSPACE}"
 AWSCLI_VERSION=$(docker inspect neovatar/aws-cli:latest | jq -r '.[0] | .ContainerConfig.Labels.AWSCLI_VERSION')
-docker tag "neovatar/aws-cli:latest neovatar/aws-cli:${AWSCLI_VERSION}"
+docker tag "neovatar/aws-cli:latest" "neovatar/aws-cli:${AWSCLI_VERSION}"
 docker push "neovatar/aws-cli:${AWSCLI_VERSION}"
