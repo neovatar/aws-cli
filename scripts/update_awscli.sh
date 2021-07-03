742f699a6b2e6d2b3c442f95af1333f0f86f8edd
Thomas Seliger <tom@neovatar.org>
2021-07-03 10:24:29 +0200

    Do git commit and tagging only after successful image build

--- scripts/update_awscli.sh
+++ scripts/update_awscli.sh
@@ -9,11 +9,11 @@ AWSCLI_CURRENT=$(grep "ARG AWSCLI_VERSION=" Dockerfile | sed -E 's/^.*=//')
 if [[ "${AWSCLI_CURRENT}" != "${AWSCLI_LATEST}" ]]; then
   echo "update awscli from ${AWSCLI_CURRENT} to ${AWSCLI_LATEST}"
   sed -i -e "s/${AWSCLI_CURRENT}/${AWSCLI_LATEST}/" Dockerfile
+  scripts/build.sh
+  scripts/push.sh
   git commit -m"AWS CLI ${AWSCLI_LATEST}" .
   git push
   # create and push the tag
   git tag --annotate "${AWSCLI_LATEST}" -m"AWS CLI ${AWSCLI_LATEST}" --force
   git push origin "${AWSCLI_LATEST}" --force
-  scripts/build.sh
-  scripts/push.sh
 fi
